f344d97cc72bc2aed9ae9ea84fa0e01ea7402cf5
Bartosz Gnatowski <86551272+bgnatowski@users.noreply.github.com>
2022-11-03 10:15:25 +0100

    skonczone + drobne bledy

--- 2.czas/czas.cpp
+++ 2.czas/czas.cpp
@@ -20,43 +20,99 @@ Czas::Czas(void){
 }
 
 Czas::Czas(int h, int m, int s, int ms){
-	normalise(h, m, s, ms);
 	setAll(h, m, s, ms);
+	normalise();
 	if (h < 0)
-		sign = true;
-	sign = false;
+		m_sign = true;
+	m_sign = false;
 }
 
 Czas::Czas(long int x){
 	if(x < 0 )
-		sign = true;
-	sign = false;
+		m_sign = true;
+	m_sign = false;
+	czasFromLongInt(x);
+	normalise();
 
-	int ms = x % 1000;
-	x /= 1000;
-	int s = x % 100;
-	x /= 100;
-	int m = x % 100;
-	x /= 100;
-	int h = x % 100;
-
-	normalise(h, m, s, ms);
-	setAll(h, m, s, ms);
 }
 
 Czas::Czas(double x){
 	if(x < 0 )
-		sign = true;
-	sign = false;
+		m_sign = true;
+	m_sign = false;
+	czasFromDouble(x);
+	normalise();
 
-	int h, m, s, ms = 0;
-	ms = x * 1000;
-	normalise(h, m, s, ms);
-	setAll(h, m, s, ms);
 }
 
-Czas::operator+=(const Czas& x){
+Czas& Czas::operator+=(const Czas& x){
+	HH+= x.h();
+	MM+= x.m();
+	SS+= x.s();
+	sss+= x.ms();
 
+	normalise();
+	return *this;
+}
+
+Czas& Czas::operator-=(const Czas& x){
+	HH-= x.h();
+	MM-= x.m();
+	SS-= x.s();
+	sss-= x.ms();
+	if(HH <= 0 && MM <= 0 && SS <= 0 && sss <= 0){
+		HH = MM = SS = sss = 0;
+		return *this;
+	}
+	normalise();
+	return *this;
+}
+
+Czas& Czas::operator=(long int x) {
+	czasFromLongInt(x);
+	return *this;
+}
+
+Czas& Czas::operator=(double x) {
+	czasFromDouble(x);
+	return *this;
+}
+
+//todo
+Czas::operator long int() const {
+	return this->convertToLongInt();
+}
+//todo
+Czas::operator double() const {
+	return this->convertToDouble();
+}
+
+Czas Czas::operator ++ (void){
+	SS++;
+	normalise();
+	return *this;
+}
+
+Czas Czas::operator -- (void){
+	SS--;
+	normalise();
+	return *this;
+}
+
+Czas Czas::operator ++ (int a){
+	Czas tmp;
+	tmp = *this;
+	SS++;
+	normalise();
+	return tmp;
+}
+
+Czas Czas::operator -- (int a){
+	Czas tmp;
+	tmp = *this;
+	SS--;
+	normalise();
+	return tmp;
 }
 //-------------------------------------------------------------
 
@@ -64,4 +120,56 @@ Czas::operator+=(const Czas& x){
 // zwi¹zane z klas¹ Czas
 
 //-------------------------------------------------------------
+
+Czas operator + (const Czas x1, const Czas x2){
+	Czas tmp = x1;
+	return tmp+=x2;
+}
+
+Czas operator - (const Czas x1, const Czas x2){
+	Czas tmp = x1;
+	return tmp-=x2;
+}
+
+bool operator > (const Czas x1, const Czas x2){
+	return ((x1.h() > x2.h())
+	&& (x1.m() > x2.m())
+	&& (x1.s() > x2.s())
+	&& (x1.ms() > x2.ms()));
+}
+
+bool operator >= (const Czas x1, const Czas x2){
+	return ((x1.h() >= x2.h())
+		&& (x1.m() >= x2.m())
+		&& (x1.s() >= x2.s())
+		&& (x1.ms() >= x2.ms()));
+}
+
+bool operator < (const Czas x1, const Czas x2){
+	return ((x1.h() < x2.h())
+		&& (x1.m() < x2.m())
+		&& (x1.s() < x2.s())
+		&& (x1.ms() < x2.ms()));
+}
+
+bool operator <= (const Czas x1, const Czas x2){
+	return ((x1.h() <= x2.h())
+		&& (x1.m() <= x2.m())
+		&& (x1.s() <= x2.s())
+		&& (x1.ms() <= x2.ms()));
+}
+
+bool operator == (const Czas x1, const Czas x2){
+	return ((x1.h() == x2.h())
+		&& (x1.m() == x2.m())
+		&& (x1.s() == x2.s())
+		&& (x1.ms() == x2.ms()));
+}
+
+bool operator != (const Czas x1, const Czas x2){
+	return ((x1.h() != x2.h())
+		&& (x1.m() != x2.m())
+		&& (x1.s() != x2.s())
+		&& (x1.ms() != x2.ms()));
+}
 
--- 2.czas/czas.h
+++ 2.czas/czas.h
@@ -3,12 +3,15 @@
 *
 * Plik czas.h
 *
-* Autorzy:
+* Autorzy: Konrad Jurczyk, Bartosz Gnatowski
 *
 ***************************************************************/
 
 #ifndef CZAS_H_INCLUDED
 #define CZAS_H_INCLUDED
+
+#include <string>
+
 
 //--------------------------------------------------------------
 
@@ -31,42 +34,83 @@ public:
 	int m(void) const { return MM; }
 	int s(void) const { return SS; }
 	int ms(void) const { return sss; }
-	bool sign(void) const{ return sign; }
+	bool sign(void) const { return m_sign; }
 	//
 	Czas & operator += (const Czas &x); // 6
 	Czas & operator -= (const Czas &x); // 7
-	Czas & operator == (long int x); // 8
-	Czas & operator == (double x); // 9
+	Czas & operator = (long int x); // 8
+	Czas & operator = (double x); // 9
 	operator long int() const; // 10
-	operator long double() const; // 11
+	operator double() const; // 11
 	// 12
-	Czas operator ++ (void);
-	Czas operator ++ (int);
+	Czas operator ++ (void); //prefix
+	Czas operator ++ (int);  //postfix
 	Czas operator -- (void);
 	Czas operator -- (int);
 
 private:
 // Tutaj nale¿y zadeklarowaæ prywatne sk³adowe klasy Czas
+	bool m_sign;
 	int HH, MM, SS, sss;
-	bool sign;
 
-	private void normalise(int &h, int &m, int &s, int &ms){
-		ms += s * 1000;
-		ms += m * 60 * 1000;
-		ms += h * 60 * 60 * 1000;
+	void normalise(void){
+		sss += SS * 1000;
+		sss += MM * 60 * 1000;
+		sss += HH * 60 * 60 * 1000;
 
-		h = ms / (60*60*1000);
-		m = ((ms / (60*1000))) % 60;
-		s = (ms / 1000) % 60;
-		ms = ms % 1000;
+		HH = sss / (60*60*1000);
+		MM = ((sss / (60*1000))) % 60;
+		SS = (sss / 1000) % 60;
+		SS = sss % 1000;
 	}
 
-	private void setAll(int h, int m, int s, int ms){
+	void setAll(int h, int m, int s, int ms){
 		HH = h;
 		MM = m;
 		SS = s;
 		sss = ms;
-	};
+	}
+
+	void czasFromLongInt(long int x){
+		sss = x % 1000;
+		x /= 1000;
+		SS = x % 100;
+		x /= 100;
+		MM = x % 100;
+		x /= 100;
+		HH = x % 100;
+	}
+
+	void czasFromDouble(double x) {
+		HH = MM = SS = sss = 0;
+		sss = x * 1000;
+	}
+
+	//todo
+	long int convertToLongInt(void){
+		long int x = 0;
+		x += HH * 10 000 000;
+		x += MM * 100 000;
+		x += SS * 1000;
+		x += sss;
+
+		if(sign())
+			return -x;
+		return x;
+	}
+
+	double convertToDouble(void) {
+		double x = 0;
+		x += HH * 60 * 60;
+		x += MM * 60;
+		x += SS;
+		x += sss * 0.001;
+
+		if(sign())
+			return -x;
+		return x;
+	}
+
 };
 
 //--------------------------------------------------------------
@@ -77,7 +121,7 @@ private:
 Czas operator + (const Czas x1, const Czas x2);
 Czas operator - (const Czas x1, const Czas x2);
 bool operator > (const Czas x1, const Czas x2);
-bool operator => (const Czas x1, const Czas x2);
+bool operator >= (const Czas x1, const Czas x2);
 bool operator < (const Czas x1, const Czas x2);
 bool operator <= (const Czas x1, const Czas x2);
 bool operator == (const Czas x1, const Czas x2);
